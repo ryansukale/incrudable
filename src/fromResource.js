e12a0ac26964feaa154ad3930e3a3f77cd373e0b
Ryan Sukale <ryansukale@gmail.com>
2018-04-29 21:51:35 -0700

    fromResource should be able to return actionGroups

--- src/fromResource.js
+++ src/fromResource.js
@@ -1,4 +1,25 @@
+import createActionGroup from './createActionGroup';
+const DEFAULT_OPERATIONS = ['create', 'read', 'update', 'del', 'list'];
+
+export function getActions(eventBase, operations) {
+  return operations.reduce((acc, operation) => {
+    acc[operation] = createActionGroup(eventBase, undefined, operation);
+    return acc;
+  }, {});
+}
+
+/**
+ * @param  {hash} resource 
+ *  {
+ *    name: 'albums',
+ *    singular: 'album',
+ *    basePath: '/data/albums',
+ *    operations: ['create', 'read']
+ *  }
+ * @return {hash} {actions, thunks}
+ */
 export default function fromResource(resource) {
-  const {name, singular, basePath, operations} = resource;
-  
+  const {name, singular, basePath, operations = DEFAULT_OPERATIONS} = resource;
+  const actions = getActions(name, operations);
+  return {actions};
 }
