e88e2d906f36a1c6d5885fa6c7303eca59cacf7a
Ryan Sukale <ryansukale@gmail.com>
2018-05-10 18:29:56 -0700

    Setub babelrc and add tests for createActionType

--- src/createActionType.js
+++ src/createActionType.js
@@ -1,5 +1,5 @@
 export default function createActionType(base, options = {}) {
-  const prefix = options.prefix ? `${prefix}_` : '';
-  const suffix = options.suffix ? `_${suffix}` : '';
+  const prefix = options.prefix ? `${options.prefix}_` : '';
+  const suffix = options.suffix ? `_${options.suffix}` : '';
   return `${prefix}${base}${suffix}`.toUpperCase();
 }
---
+++ tests/createActionType.test.js
@@ -0,0 +1,32 @@
+import {expect} from 'chai';
+
+import createActionType from '../src/createActionType';
+
+describe('createActionType', function () {
+  it('returns a string in the action type format', function () {
+    const options = {
+      prefix: 'create',
+      suffix: 'success'
+    };
+    expect(createActionType('albums', options)).to.equal('CREATE_ALBUMS_SUCCESS');
+  });
+
+  it('returns a string if suffix isint passed', function () {
+    const options = {
+      prefix: 'create'
+    };
+    expect(createActionType('albums', options)).to.equal('CREATE_ALBUMS');
+  });
+
+  it('returns a string if prefix isint passed', function () {
+    const options = {
+      prefix: 'create',
+      suffix: 'success'
+    };
+    expect(createActionType('albums', options)).to.equal('CREATE_ALBUMS_SUCCESS');
+  });
+
+  it('returns a string when options are not prvided', function () {
+    expect(createActionType('albums')).to.equal('ALBUMS');
+  });
+});
