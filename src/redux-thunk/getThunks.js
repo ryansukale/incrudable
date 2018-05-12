9f1b76e6d60f549cb03ec3a598c884cd457ec697
Ryan Sukale <ryansukale@gmail.com>
2018-05-12 16:03:57 -0700

    Create a skeleton getTaskName function

--- src/redux-thunk/getThunks.js
+++ src/redux-thunk/getThunks.js
@@ -1,8 +1,15 @@
 // import createActionGroup from './createActionGroup';
 
+function getTaskName(resource, operation) {
+  // TODO transform to camel case names like 
+  // createAlbum, readAlbum, listAlbums etc
+  return operation;
+}
+
 export default function getThunks(resource, actions) {
   return Object.keys(actions).reduce((acc, operation) => {
-    acc[operation] = 'TODO';
+    const taskName = getTaskName(resource, operation);
+    acc[taskName] = 'TODO';
     return acc;
   }, {});
   // return operations.reduce((acc, operation) => {
