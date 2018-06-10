fb154a07ab6f999885a0137dbd67671885c44420
Ryan Sukale <ryansukale@gmail.com>
2018-06-09 17:40:41 -0700

    Support all the operations in generateEpic

--- src/redux-observable/generateEpic.js
+++ src/redux-observable/generateEpic.js
@@ -2,47 +2,107 @@ import { Observable, from, of } from 'rxjs';
 import { map, filter, switchMap, catchError } from 'rxjs/operators';
 // import { combineEpics } from 'redux-observable';
 
-const epicGenerators = {
-  create({ url, actions, onSuccess, onFailure }, { ajax }) {
-    function task(request) {
-      return actions.wait(request);
-    }
+function epicGenerator(ajaxMethodName, config, { ajax }) {
+  const {
+    url,
+    actions,
+    onSuccess,
+    onFailure
+  } = config;
 
-    function submit({payload: request}) {
-      return from(ajax.getJSON(request))
-        .pipe(
-          map(
-            (response) => onJsonApiResponse({actions, payload: {request, response}})
-          ),
-          catchError(
-            (response) => of(
-              onJsonApiError({actions, payload: {request, response}})
-            )
-          )
-        );
-    }
-
-    function onJsonApiResponse({actions, payload}) {
-      return actions.success(payload);
-    }
-
-    function onJsonApiError({actions, payload}) {
-      return actions.failure(payload);
-    }
-
-    function epic(action$) {
-      return action$
-        .pipe(
-          filter(actions.wait),
-          switchMap(submit)
-        );
-    }
-    
-    task.epic = epic;
-
-    return task;
+  function task(request) {
+    return actions.wait(request);
   }
+
+  function submit({payload: request}) {
+    return from(ajax[ajaxMethodName](request))
+      .pipe(
+        map(
+          (response) => onJsonApiResponse({actions, payload: {request, response}})
+        ),
+        catchError(
+          (response) => of(
+            onJsonApiError({actions, payload: {request, response}})
+          )
+        )
+      );
+  }
+
+  function onJsonApiResponse({actions, payload}) {
+    return actions.success(payload);
+  }
+
+  function onJsonApiError({actions, payload}) {
+    return actions.failure(payload);
+  }
+
+  function epic(action$) {
+    return action$
+      .pipe(
+        filter(actions.wait),
+        switchMap(submit)
+      );
+  }
+  
+  task.epic = epic;
+
+  return task;
+}
+
+const create = epicGenerator.bind(null, 'postJSON');
+const read = epicGenerator.bind(null, 'getJSON');
+const update = epicGenerator.bind(null, 'putJSON');
+const del = epicGenerator.bind(null, 'delJSON');
+
+const epicGenerators = {
+  create,
+  read,
+  update,
+  del,
+  list: read
 };
+
+// const epicGenerators = {
+//   create({ url, actions, onSuccess, onFailure }, { ajax }) {
+//     function task(request) {
+//       return actions.wait(request);
+//     }
+
+//     function submit({payload: request}) {
+//       return from(ajax.getJSON(request))
+//         .pipe(
+//           map(
+//             (response) => onJsonApiResponse({actions, payload: {request, response}})
+//           ),
+//           catchError(
+//             (response) => of(
+//               onJsonApiError({actions, payload: {request, response}})
+//             )
+//           )
+//         );
+//     }
+
+//     function onJsonApiResponse({actions, payload}) {
+//       return actions.success(payload);
+//     }
+
+//     function onJsonApiError({actions, payload}) {
+//       return actions.failure(payload);
+//     }
+
+//     function epic(action$) {
+//       return action$
+//         .pipe(
+//           filter(actions.wait),
+//           switchMap(submit)
+//         );
+//     }
+    
+//     task.epic = epic;
+
+//     return task;
+//   }
+// };
 
 export default function generateEpic(
   { operation, actions, onSuccess, onFailure, url },
--- tests/redux-observable/generateEpic.test.js
+++ tests/redux-observable/generateEpic.test.js
@@ -6,8 +6,7 @@ import sinon from 'sinon';
 import createActionGroup from '../../src/createActionGroup';
 import generateEpic from '../../src/redux-observable/generateEpic';
 
-describe('generateEpic', () => {
-
+describe.only('generateEpic', () => {
   function getTask(options, ajax) {
     sinon.spy(options.actions, 'wait');
     sinon.spy(options.actions, 'success');
@@ -27,10 +26,10 @@ describe('generateEpic', () => {
       };
       const request = { body: 'hello' };
       const response = { body: 'test'};
-      const ajax = { getJSON: () => Promise.resolve(response) };
-      const create = getTask(options, ajax);
-      const {epic} = create;
-      const action$ = of(create(request));
+      const ajax = { postJSON: () => Promise.resolve(response) };
+      const operation = getTask(options, ajax);
+      const {epic} = operation;
+      const action$ = of(operation(request));
 
       epic(action$).subscribe(() => {
         expect(options.actions.wait.args[0][0]).to.deep.equal(request);
@@ -47,10 +46,178 @@ describe('generateEpic', () => {
       };
       const request = { body: 'hello' };
       const response = { body: 'Test error'};
+      const ajax = { postJSON: () => Promise.reject(response) };
+      const operation = getTask(options, ajax);
+      const {epic} = operation;
+      const action$ = of(operation(request));
+
+      epic(action$).subscribe(() => {
+        expect(options.actions.wait.args[0][0]).to.deep.equal(request);
+        expect(options.actions.failure.args[0][0]).to.deep.equal({request, response});
+        done();
+      });
+    })
+  });
+
+  describe('"read" operation', () => {
+    it('invokes the success action on ajax success', (done) => {
+      const options = {
+        operation: 'read',
+        actions: createActionGroup('READ_ALBUMS'),
+        url: '/albums'
+      };
+      const request = { body: 'hello' };
+      const response = { body: 'test'};
+      const ajax = { getJSON: () => Promise.resolve(response) };
+      const operation = getTask(options, ajax);
+      const {epic} = operation;
+      const action$ = of(operation(request));
+
+      epic(action$).subscribe(() => {
+        expect(options.actions.wait.args[0][0]).to.deep.equal(request);
+        expect(options.actions.success.args[0][0]).to.deep.equal({request, response});
+        done();
+      });
+    })
+
+    it('invokes the failure action on ajax error', (done) => {
+      const options = {
+        operation: 'read',
+        actions: createActionGroup('READ_ALBUMS'),
+        url: '/albums'
+      };
+      const request = { body: 'hello' };
+      const response = { body: 'Test error'};
       const ajax = { getJSON: () => Promise.reject(response) };
-      const create = getTask(options, ajax);
-      const {epic} = create;
-      const action$ = of(create(request));
+      const operation = getTask(options, ajax);
+      const {epic} = operation;
+      const action$ = of(operation(request));
+
+      epic(action$).subscribe(() => {
+        expect(options.actions.wait.args[0][0]).to.deep.equal(request);
+        expect(options.actions.failure.args[0][0]).to.deep.equal({request, response});
+        done();
+      });
+    })
+  });
+
+  describe('"update" operation', () => {
+    it('invokes the success action on ajax success', (done) => {
+      const options = {
+        operation: 'update',
+        actions: createActionGroup('UPDATE_ALBUMS'),
+        url: '/albums'
+      };
+      const request = { body: 'hello' };
+      const response = { body: 'test'};
+      const ajax = { putJSON: () => Promise.resolve(response) };
+      const operation = getTask(options, ajax);
+      const {epic} = operation;
+      const action$ = of(operation(request));
+
+      epic(action$).subscribe(() => {
+        expect(options.actions.wait.args[0][0]).to.deep.equal(request);
+        expect(options.actions.success.args[0][0]).to.deep.equal({request, response});
+        done();
+      });
+    })
+
+    it('invokes the failure action on ajax error', (done) => {
+      const options = {
+        operation: 'update',
+        actions: createActionGroup('UPDATE_ALBUMS'),
+        url: '/albums'
+      };
+      const request = { body: 'hello' };
+      const response = { body: 'Test error'};
+      const ajax = { putJSON: () => Promise.reject(response) };
+      const operation = getTask(options, ajax);
+      const {epic} = operation;
+      const action$ = of(operation(request));
+
+      epic(action$).subscribe(() => {
+        expect(options.actions.wait.args[0][0]).to.deep.equal(request);
+        expect(options.actions.failure.args[0][0]).to.deep.equal({request, response});
+        done();
+      });
+    })
+  });
+
+  describe('"del" operation', () => {
+    it('invokes the success action on ajax success', (done) => {
+      const options = {
+        operation: 'del',
+        actions: createActionGroup('DEL_ALBUMS'),
+        url: '/albums'
+      };
+      const request = { body: 'hello' };
+      const response = { body: 'test'};
+      const ajax = { delJSON: () => Promise.resolve(response) };
+      const operation = getTask(options, ajax);
+      const {epic} = operation;
+      const action$ = of(operation(request));
+
+      epic(action$).subscribe(() => {
+        expect(options.actions.wait.args[0][0]).to.deep.equal(request);
+        expect(options.actions.success.args[0][0]).to.deep.equal({request, response});
+        done();
+      });
+    })
+
+    it('invokes the failure action on ajax error', (done) => {
+      const options = {
+        operation: 'del',
+        actions: createActionGroup('DEL_ALBUMS'),
+        url: '/albums'
+      };
+      const request = { body: 'hello' };
+      const response = { body: 'Test error'};
+      const ajax = { delJSON: () => Promise.reject(response) };
+      const operation = getTask(options, ajax);
+      const {epic} = operation;
+      const action$ = of(operation(request));
+
+      epic(action$).subscribe(() => {
+        expect(options.actions.wait.args[0][0]).to.deep.equal(request);
+        expect(options.actions.failure.args[0][0]).to.deep.equal({request, response});
+        done();
+      });
+    })
+  });
+
+  describe('"list" operation', () => {
+    it('invokes the success action on ajax success', (done) => {
+      const options = {
+        operation: 'list',
+        actions: createActionGroup('LIST_ALBUMS'),
+        url: '/albums'
+      };
+      const request = { body: 'hello' };
+      const response = { body: 'test'};
+      const ajax = { getJSON: () => Promise.resolve(response) };
+      const operation = getTask(options, ajax);
+      const {epic} = operation;
+      const action$ = of(operation(request));
+
+      epic(action$).subscribe(() => {
+        expect(options.actions.wait.args[0][0]).to.deep.equal(request);
+        expect(options.actions.success.args[0][0]).to.deep.equal({request, response});
+        done();
+      });
+    })
+
+    it('invokes the failure action on ajax error', (done) => {
+      const options = {
+        operation: 'list',
+        actions: createActionGroup('LIST_ALBUMS'),
+        url: '/albums'
+      };
+      const request = { body: 'hello' };
+      const response = { body: 'Test error'};
+      const ajax = { getJSON: () => Promise.reject(response) };
+      const operation = getTask(options, ajax);
+      const {epic} = operation;
+      const action$ = of(operation(request));
 
       epic(action$).subscribe(() => {
         expect(options.actions.wait.args[0][0]).to.deep.equal(request);
