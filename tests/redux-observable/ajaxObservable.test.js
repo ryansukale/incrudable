cb131f22dec36c8297b0fa4574a199a448a5797b
Ryan Sukale <ryansukale@gmail.com>
2018-06-11 10:46:54 -0700

    Mock the post test for ajaxobservable.postJSON

--- tests/redux-observable/ajaxObservable.test.js
+++ tests/redux-observable/ajaxObservable.test.js
@@ -2,6 +2,14 @@
 import { expect } from 'chai';
 import sinon from 'sinon';
 import mock from 'xhr-mock';
+import {JSDOM} from 'jsdom';
+import {Blob} from 'blob-polyfill';
+import FormData from 'form-data';
+
+global.window = (new JSDOM()).window;
+global.Document = window.Document;
+global.Blob = Blob;
+global.FormData = FormData;
 
 import ajaxObservable from '../../src/redux-observable/ajaxObservable';
 const path = '/albums';
@@ -24,6 +32,23 @@ describe('ajaxObservable', () => {
 
     ajaxObservable
       .getJSON(path)
+      .subscribe(data => {
+        expect(data).to.deep.equal(responseBody);
+        done();
+      });
+  });
+
+  it('returns an observable of a GET request', (done) => {
+    const responseBody = {hello: 'world'};
+    const request = {body: 'message'};
+    mock.post(path, {
+      status: 201,
+      reason: 'Created',
+      body: JSON.stringify(responseBody)
+    });
+
+    ajaxObservable
+      .postJSON(path, request)
       .subscribe(data => {
         expect(data).to.deep.equal(responseBody);
         done();
