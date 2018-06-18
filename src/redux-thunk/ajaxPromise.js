21ec9627ddf040e2fcebbbc0c688f6dc556eb68c
Ryan Sukale <ryansukale@gmail.com>
2018-06-18 09:59:12 -0700

    Refactor out a  wrapper ajax function for ajaxPromise

--- src/redux-thunk/ajaxPromise.js
+++ src/redux-thunk/ajaxPromise.js
@@ -1,25 +1,55 @@
 import fetch from 'whatwg-fetch';
-// FUTURE: Refactor this
+
+const DEAULT_HEADERS = {
+  'Accept': 'application/json',
+  'Content-Type': 'application/json; charset=utf-8'
+};
+
+function onResponse(response) {
+  if (response.ok) {
+    return response.json();
+  }
+
+  if (response.headers.get('Content-Type').indexOf('json') !== -1) {
+    return response
+      .json()
+      .then((error) => Promise.reject(error));
+  }
+
+  return Promise.reject(response);
+}
+
+export function ajax(url, options) {
+  return fetch(url, {
+    ...options,
+    headers: {
+      ...DEAULT_HEADERS,
+      ...options.headers
+    }
+  }).then(onResponse);
+}
 
 export default {
   getJSON(url) {
-    return fetch(url).then(response => response.json());
+    return ajax(url);
   },
   postJSON(url, { body, ...options }) {
-    return fetch(url, {
+    return ajax(url, {
       method: 'POST',
       body: JSON.stringify(options.body),
       ...options
-    }).then(response => response.json());
+    });
   },
   putJSON(url, { body, ...options }) {
-    return fetch(url, {
-      method: 'POST',
+    return ajax(url, {
+      method: 'PUT',
       body: JSON.stringify(body),
       ...options
-    }).then(response => response.json());
+    });
   },
   delJSON(url) {
-    return fetch(url).then(response => response.json());
+    return ajax(url, {
+      method: 'DELETE'
+    });
   }
 };
