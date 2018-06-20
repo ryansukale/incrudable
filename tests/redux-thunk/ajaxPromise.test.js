bfe1d9e91660294e7d15381abf2a95edc3a2a246
Ryan Sukale <ryansukale@gmail.com>
2018-06-20 09:56:12 -0700

    Added all tests for ajaxPromise

--- tests/redux-thunk/ajaxPromise.test.js
+++ tests/redux-thunk/ajaxPromise.test.js
@@ -8,22 +8,24 @@ const path = 'http://example.com/albums';
 describe('ajaxPromise', () => {
   let prevFetch;
   let responseBody;
+  let fetchSpy;
   const mockFetchResponse = Promise.resolve({
     ok: true,
     json: () => Promise.resolve(responseBody)
   });
-  const fetchSpy = sinon.spy(() => mockFetchResponse);
+  const createFetchSpy = () => sinon.spy(() => mockFetchResponse);
 
-  before(() => {
+  beforeEach(() => {
     prevFetch = global.fetch;
+    fetchSpy = createFetchSpy();
     global.fetch = fetchSpy;
   });
 
-  after(() => {
+  afterEach(() => {
     global.fetch = prevFetch;
   });
 
-  it.only('returns an observable of a GET request', () => {
+  it('returns an observable of a GET request', () => {
     responseBody = {hello: 'world'};
 
     return ajaxPromise()
@@ -40,48 +42,64 @@ describe('ajaxPromise', () => {
       });
   });
 
-  it('returns an observable of a POST request', (done) => {
-    const responseBody = {hello: 'world'};
-    const request = {body: 'message'};
-    mock.post(path, {
-      status: 201,
-      body: JSON.stringify(responseBody)
-    });
+  it('returns an observable of a POST request', () => {
+    responseBody = {hello: 'world'};
+    const body = {x: 10};
 
-    ajaxPromise
-      .postJSON(path, request)
-      .subscribe(data => {
+    return ajaxPromise()
+      .postJSON(path, {body})
+      .then(data => {
         expect(data).to.deep.equal(responseBody);
-        done();
+        expect(fetchSpy.getCalls().length).to.equal(1);
+
+        const args = fetchSpy.args[0];
+        
+        expect(args[0]).to.equal(path);
+        expect(args[1]).to.deep.equal({
+          method: 'POST',
+          body: JSON.stringify(body),
+          headers: DEAULT_HEADERS
+        });
       });
   });
 
-  it('returns an observable of a PUT request', (done) => {
-    const responseBody = {hello: 'world'};
-    const request = {body: 'message'};
-    mock.put(path, {
-      status: 200,
-      body: JSON.stringify(responseBody)
-    });
+  it('returns an observable of a PUT request', () => {
+    responseBody = {hello: 'world'};
+    const body = {x: 10};
 
-    ajaxPromise
-      .putJSON(path, request)
-      .subscribe(data => {
+    return ajaxPromise()
+      .putJSON(path, {body})
+      .then(data => {
         expect(data).to.deep.equal(responseBody);
-        done();
+        expect(fetchSpy.getCalls().length).to.equal(1);
+
+        const args = fetchSpy.args[0];
+        
+        expect(args[0]).to.equal(path);
+        expect(args[1]).to.deep.equal({
+          method: 'PUT',
+          body: JSON.stringify(body),
+          headers: DEAULT_HEADERS
+        });
       });
   });
 
-  it('returns an observable of a DELETE request', (done) => {
-    mock.delete(path, {
-      status: 200
-    });
-
-    ajaxPromise
+  it('returns an observable of a DELETE request', () => {
+    responseBody = undefined;
+    
+    return ajaxPromise()
       .delJSON(path)
-      .subscribe(data => {
-        expect(data).to.deep.equal('');
-        done();
+      .then(data => {
+        expect(data).to.deep.equal(responseBody);
+        expect(fetchSpy.getCalls().length).to.equal(1);
+
+        const args = fetchSpy.args[0];
+        
+        expect(args[0]).to.equal(path);
+        expect(args[1]).to.deep.equal({
+          method: 'DELETE',
+          headers: DEAULT_HEADERS
+        });
       });
   });
 });
