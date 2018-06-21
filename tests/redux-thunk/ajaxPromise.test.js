4e1e48010e4c1f73a4122179a40525f3cc3cbc66
Ryan Sukale <ryansukale@gmail.com>
2018-06-20 17:33:21 -0700

    Test usage of custom headers in ajaxPromise, refactor its tests

--- tests/redux-thunk/ajaxPromise.test.js
+++ tests/redux-thunk/ajaxPromise.test.js
@@ -14,10 +14,29 @@ describe('ajaxPromise', () => {
     json: () => Promise.resolve(responseBody)
   });
   const createFetchSpy = () => sinon.spy(() => mockFetchResponse);
+  const customHeaders = {
+    'X-CUSTOM-HEADER': 'value'
+  };
+
+  function getHeaders() {
+    return customHeaders;
+  }
+
+  function assertCustomHeader(methodName, options) {
+    return ajaxPromise(getHeaders)[methodName](path, options)
+      .then(data => {
+        const args = fetchSpy.args[0];
+        expect(args[1].headers).to.deep.equal({
+          ...DEAULT_HEADERS,
+          ...customHeaders
+        });
+      });
+  }
 
   beforeEach(() => {
     prevFetch = global.fetch;
     fetchSpy = createFetchSpy();
+    responseBody = undefined;
     global.fetch = fetchSpy;
   });
 
@@ -25,81 +44,110 @@ describe('ajaxPromise', () => {
     global.fetch = prevFetch;
   });
 
-  it('returns an observable of a GET request', () => {
-    responseBody = {hello: 'world'};
+  describe('getJSON', () => {
+    it('invokes fetch with the params of GET request', () => {
+      responseBody = {hello: 'world'};
 
-    return ajaxPromise()
-      .getJSON(path)
-      .then(data => {
-        expect(data).to.deep.equal(responseBody);
-        expect(fetchSpy.getCalls().length).to.equal(1);
+      return ajaxPromise()
+        .getJSON(path)
+        .then(data => {
+          expect(data).to.deep.equal(responseBody);
+          expect(fetchSpy.getCalls().length).to.equal(1);
 
-        const args = fetchSpy.args[0];
-        expect(args[0]).to.equal(path);
-        expect(args[1]).to.deep.equal({
-          headers: DEAULT_HEADERS
+          const args = fetchSpy.args[0];
+          expect(args[0]).to.equal(path);
+          expect(args[1]).to.deep.equal({
+            headers: DEAULT_HEADERS
+          });
         });
-      });
+    });
+
+    it('uses custom headers in the GET request', () => {
+      return assertCustomHeader('getJSON');
+    });
   });
 
-  it('returns an observable of a POST request', () => {
-    responseBody = {hello: 'world'};
-    const body = {x: 10};
+  describe('postJSON', () => {
+    it('invokes fetch with the params of POST request', () => {
+      responseBody = {hello: 'world'};
+      const body = {x: 10};
 
-    return ajaxPromise()
-      .postJSON(path, {body})
-      .then(data => {
-        expect(data).to.deep.equal(responseBody);
-        expect(fetchSpy.getCalls().length).to.equal(1);
+      return ajaxPromise()
+        .postJSON(path, {body})
+        .then(data => {
+          expect(data).to.deep.equal(responseBody);
+          expect(fetchSpy.getCalls().length).to.equal(1);
 
-        const args = fetchSpy.args[0];
-        
-        expect(args[0]).to.equal(path);
-        expect(args[1]).to.deep.equal({
-          method: 'POST',
-          body: JSON.stringify(body),
-          headers: DEAULT_HEADERS
+          const args = fetchSpy.args[0];
+          
+          expect(args[0]).to.equal(path);
+          expect(args[1]).to.deep.equal({
+            method: 'POST',
+            body: JSON.stringify(body),
+            headers: DEAULT_HEADERS
+          });
         });
-      });
+    });
+
+    it('uses custom headers in the POST request', () => {
+      responseBody = {hello: 'world'};
+      const body = {x: 10};
+      return assertCustomHeader('postJSON', {body});
+    });
   });
 
-  it('returns an observable of a PUT request', () => {
-    responseBody = {hello: 'world'};
-    const body = {x: 10};
+  describe('putJSON', () => {
+    it('invokes fetch with the params of PUT request', () => {
+      responseBody = {hello: 'world'};
+      const body = {x: 10};
 
-    return ajaxPromise()
-      .putJSON(path, {body})
-      .then(data => {
-        expect(data).to.deep.equal(responseBody);
-        expect(fetchSpy.getCalls().length).to.equal(1);
+      return ajaxPromise()
+        .putJSON(path, {body})
+        .then(data => {
+          expect(data).to.deep.equal(responseBody);
+          expect(fetchSpy.getCalls().length).to.equal(1);
 
-        const args = fetchSpy.args[0];
-        
-        expect(args[0]).to.equal(path);
-        expect(args[1]).to.deep.equal({
-          method: 'PUT',
-          body: JSON.stringify(body),
-          headers: DEAULT_HEADERS
+          const args = fetchSpy.args[0];
+          
+          expect(args[0]).to.equal(path);
+          expect(args[1]).to.deep.equal({
+            method: 'PUT',
+            body: JSON.stringify(body),
+            headers: DEAULT_HEADERS
+          });
         });
-      });
+    });
+
+    it('uses custom headers in the PUT request', () => {
+      responseBody = {hello: 'world'};
+      const body = {x: 10};
+      return assertCustomHeader('putJSON', {body});
+    });
   });
 
-  it('returns an observable of a DELETE request', () => {
-    responseBody = undefined;
-    
-    return ajaxPromise()
-      .delJSON(path)
-      .then(data => {
-        expect(data).to.deep.equal(responseBody);
-        expect(fetchSpy.getCalls().length).to.equal(1);
+  describe('delJSON', () => {
+    it('invokes fetch with the params of DELETE request', () => {
+      responseBody = undefined;
+      
+      return ajaxPromise()
+        .delJSON(path)
+        .then(data => {
+          expect(data).to.deep.equal(responseBody);
+          expect(fetchSpy.getCalls().length).to.equal(1);
 
-        const args = fetchSpy.args[0];
-        
-        expect(args[0]).to.equal(path);
-        expect(args[1]).to.deep.equal({
-          method: 'DELETE',
-          headers: DEAULT_HEADERS
+          const args = fetchSpy.args[0];
+          
+          expect(args[0]).to.equal(path);
+          expect(args[1]).to.deep.equal({
+            method: 'DELETE',
+            headers: DEAULT_HEADERS
+          });
         });
-      });
+    });
+
+    it('uses custom headers in the DELETE request', () => {
+      responseBody = undefined;
+      return assertCustomHeader('delJSON');
+    });
   });
 });
