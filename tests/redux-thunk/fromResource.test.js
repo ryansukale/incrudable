bb223953be6765fd79f5ab2fd1ea3b0d0b9d1cac
Ryan Sukale <ryansukale@gmail.com>
2018-06-30 18:07:48 -0700

    Fix lint

--- tests/redux-thunk/fromResource.test.js
+++ tests/redux-thunk/fromResource.test.js
@@ -1,11 +1,9 @@
-/* global describe, it */
+/* global describe, it, beforeEach */
 import { expect } from 'chai';
 import sinon from 'sinon';
 
 import fromResource from '../../src/redux-thunk/fromResource';
-// import createActionGroup from '../../src/createActionGroup';
 
-// const actionNames = ['success', 'failure', 'wait'];
 function createMockAjax() {
   return {
     postJSON: sinon.stub().returns(Promise.resolve('test postJSON')),
@@ -21,9 +19,9 @@ describe('redux-thunk: fromResource', () => {
     name: 'songs',
     operations: {
       create: '/albums/:id/songs',
-      read: '/albums/:id/songs/:song_id',
-      update: '/albums/:id/songs/:song_id',
-      del: '/albums/:id/songs/:song_id',
+      read: '/albums/:id/songs/:songId',
+      update: '/albums/:id/songs/:songId',
+      del: '/albums/:id/songs/:songId',
       list: '/albums/:id/songs'
     }
   };
@@ -36,41 +34,57 @@ describe('redux-thunk: fromResource', () => {
     const dispatch = sinon.spy();
     const request = { body: 'hello', params: { id: 10 } };
 
-    return thunks.create(request)(dispatch).then(() => {
-      expect(dispatch.calledTwice).to.equal(true);
-      expect(dispatch.getCall(0).args).to.deep.equal([{
-        type: 'CREATE_SONGS_WAIT',
-        payload: { request }
-      }]);
-      expect(dispatch.getCall(1).args).to.deep.equal([{
-        type: 'CREATE_SONGS_SUCCESS',
-        payload: { request, response: 'test postJSON' }
-      }]);
+    return thunks
+      .create(request)(dispatch)
+      .then(() => {
+        expect(dispatch.calledTwice).to.equal(true);
+        expect(dispatch.getCall(0).args).to.deep.equal([
+          {
+            type: 'CREATE_SONGS_WAIT',
+            payload: { request }
+          }
+        ]);
+        expect(dispatch.getCall(1).args).to.deep.equal([
+          {
+            type: 'CREATE_SONGS_SUCCESS',
+            payload: { request, response: 'test postJSON' }
+          }
+        ]);
 
-      expect(config.ajax.postJSON.calledOnce);
-      expect(config.ajax.postJSON.firstCall.args[0]).to.equal('/albums/10/songs');
-    });
+        expect(config.ajax.postJSON.calledOnce);
+        expect(config.ajax.postJSON.firstCall.args[0]).to.equal(
+          '/albums/10/songs'
+        );
+      });
   });
 
   it('generates a READ thunk for a resource with actions', () => {
     const thunks = fromResource(resource, config);
     const dispatch = sinon.spy();
-    const request = { params: {id: 10, song_id: 20} };
+    const request = { params: { id: 10, songId: 20 } };
 
-    return thunks.read(request)(dispatch).then(() => {
-      expect(dispatch.calledTwice).to.equal(true);
-      expect(dispatch.getCall(0).args).to.deep.equal([{
-        type: 'READ_SONGS_WAIT',
-        payload: { request }
-      }]);
-      expect(dispatch.getCall(1).args).to.deep.equal([{
-        type: 'READ_SONGS_SUCCESS',
-        payload: { request, response: 'test getJSON' }
-      }]);
+    return thunks
+      .read(request)(dispatch)
+      .then(() => {
+        expect(dispatch.calledTwice).to.equal(true);
+        expect(dispatch.getCall(0).args).to.deep.equal([
+          {
+            type: 'READ_SONGS_WAIT',
+            payload: { request }
+          }
+        ]);
+        expect(dispatch.getCall(1).args).to.deep.equal([
+          {
+            type: 'READ_SONGS_SUCCESS',
+            payload: { request, response: 'test getJSON' }
+          }
+        ]);
 
-      expect(config.ajax.getJSON.calledOnce);
-      expect(config.ajax.getJSON.firstCall.args[0]).to.equal('/albums/10/songs/20');
-    });
+        expect(config.ajax.getJSON.calledOnce);
+        expect(config.ajax.getJSON.firstCall.args[0]).to.equal(
+          '/albums/10/songs/20'
+        );
+      });
   });
 
   it('generates a UPDATE thunk for a resource with actions', () => {
@@ -78,41 +92,57 @@ describe('redux-thunk: fromResource', () => {
     const dispatch = sinon.spy();
     const request = { body: 'hello' };
 
-    return thunks.update(request)(dispatch).then(() => {
-      expect(dispatch.calledTwice).to.equal(true);
-      expect(dispatch.getCall(0).args).to.deep.equal([{
-        type: 'UPDATE_SONGS_WAIT',
-        payload: { request }
-      }]);
-      expect(dispatch.getCall(1).args).to.deep.equal([{
-        type: 'UPDATE_SONGS_SUCCESS',
-        payload: { request, response: 'test putJSON' }
-      }]);
+    return thunks
+      .update(request)(dispatch)
+      .then(() => {
+        expect(dispatch.calledTwice).to.equal(true);
+        expect(dispatch.getCall(0).args).to.deep.equal([
+          {
+            type: 'UPDATE_SONGS_WAIT',
+            payload: { request }
+          }
+        ]);
+        expect(dispatch.getCall(1).args).to.deep.equal([
+          {
+            type: 'UPDATE_SONGS_SUCCESS',
+            payload: { request, response: 'test putJSON' }
+          }
+        ]);
 
-      expect(config.ajax.putJSON.calledOnce);
-      expect(config.ajax.putJSON.firstCall.args[0]).to.equal(resource.operations.update);
-    });
+        expect(config.ajax.putJSON.calledOnce);
+        expect(config.ajax.putJSON.firstCall.args[0]).to.equal(
+          resource.operations.update
+        );
+      });
   });
 
   it('generates a DELETE thunk for a resource with actions', () => {
     const thunks = fromResource(resource, config);
     const dispatch = sinon.spy();
-    const request = { params: {id: 10, song_id: 20} };
+    const request = { params: { id: 10, songId: 20 } };
 
-    return thunks.del(request)(dispatch).then(() => {
-      expect(dispatch.calledTwice).to.equal(true);
-      expect(dispatch.getCall(0).args).to.deep.equal([{
-        type: 'DEL_SONGS_WAIT',
-        payload: { request }
-      }]);
-      expect(dispatch.getCall(1).args).to.deep.equal([{
-        type: 'DEL_SONGS_SUCCESS',
-        payload: { request, response: 'test delJSON' }
-      }]);
+    return thunks
+      .del(request)(dispatch)
+      .then(() => {
+        expect(dispatch.calledTwice).to.equal(true);
+        expect(dispatch.getCall(0).args).to.deep.equal([
+          {
+            type: 'DEL_SONGS_WAIT',
+            payload: { request }
+          }
+        ]);
+        expect(dispatch.getCall(1).args).to.deep.equal([
+          {
+            type: 'DEL_SONGS_SUCCESS',
+            payload: { request, response: 'test delJSON' }
+          }
+        ]);
 
-      expect(config.ajax.delJSON.calledOnce);
-      expect(config.ajax.delJSON.firstCall.args[0]).to.equal('/albums/10/songs/20');
-    });
+        expect(config.ajax.delJSON.calledOnce);
+        expect(config.ajax.delJSON.firstCall.args[0]).to.equal(
+          '/albums/10/songs/20'
+        );
+      });
   });
 
   it('generates a LIST thunk for a resource with actions', () => {
@@ -120,20 +150,27 @@ describe('redux-thunk: fromResource', () => {
     const dispatch = sinon.spy();
     const request = { body: 'hello', params: { id: 10 } };
 
-    return thunks.list(request)(dispatch).then(() => {
-      expect(dispatch.calledTwice).to.equal(true);
-      expect(dispatch.getCall(0).args).to.deep.equal([{
-        type: 'LIST_SONGS_WAIT',
-        payload: { request }
-      }]);
-      expect(dispatch.getCall(1).args).to.deep.equal([{
-        type: 'LIST_SONGS_SUCCESS',
-        payload: { request, response: 'test getJSON' }
-      }]);
+    return thunks
+      .list(request)(dispatch)
+      .then(() => {
+        expect(dispatch.calledTwice).to.equal(true);
+        expect(dispatch.getCall(0).args).to.deep.equal([
+          {
+            type: 'LIST_SONGS_WAIT',
+            payload: { request }
+          }
+        ]);
+        expect(dispatch.getCall(1).args).to.deep.equal([
+          {
+            type: 'LIST_SONGS_SUCCESS',
+            payload: { request, response: 'test getJSON' }
+          }
+        ]);
 
-      expect(config.ajax.getJSON.calledOnce);
-      expect(config.ajax.getJSON.firstCall.args[0]).to.equal('/albums/10/songs');
-    });
+        expect(config.ajax.getJSON.calledOnce);
+        expect(config.ajax.getJSON.firstCall.args[0]).to.equal(
+          '/albums/10/songs'
+        );
+      });
   });
 });
-
