8b2d79e593a02d030119c8837381d0c4d2c6d96b
Ryan Sukale <ryansukale@gmail.com>
2018-07-04 22:04:27 -0700

    Add fromResource to as a prop on incrudable

--- src/redux-observable/index.js
+++ src/redux-observable/index.js
@@ -1,6 +1,9 @@
 import processResources from '../processResources';
 import generateEpic from './generateEpic';
+import fromResource from './fromResource';
 
 export default function incrudable(resources, config) {
   return processResources(generateEpic, resources, config);
 }
+
+incrudable.fromResource = fromResource;
